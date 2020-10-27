67a5ecfba40168d24db256254cab7380d1772b8f
Prudence Gaboriau <prudence.gaboriau@ext.vallourec.com>
2020-10-27 12:44:27 +0100

    feat(master): call of all component passing input and use output event emitter to trigger users updates

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,51 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UserInformationsService } from './services/user-informations.service';
+import { User } from 'src/interfaces/user.interface';
+import { Key } from 'src/interfaces/key.interface';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'vid-test';
+export class AppComponent implements OnInit {
+  users: User[] = [];
+  filteredUsers: User[];
+  keys: Key[];
+
+  constructor(
+    private userInformationService: UserInformationsService,
+  ) {}
+
+  ngOnInit(): void {
+    const users = this.userInformationService.getUsers();
+    const sortedUsers = this.userInformationService.sortByProperty('lastname', users);
+
+    if (!users) {
+      localStorage.setItem('users', JSON.stringify([]));
+    }
+
+    this.users = sortedUsers;
+    this.filteredUsers = sortedUsers;
+    this.keys = this.userInformationService.getKeys();
+  }
+
+  addUser(user: User): void {
+    const result = this.userInformationService.saveUser(user, this.users);
+    const newUsers = result ? result : this.users;
+
+    this.users = newUsers;
+    this.filteredUsers = result
+      ? this.userInformationService.addUser(user, this.filteredUsers)
+      : this.filteredUsers
+    ;
+  }
+
+  deleteUsers(newUsersList: User[]): void {
+    this.users = newUsersList;
+  }
+
+  updateUsers(newUsersList: User[]): void {
+    this.filteredUsers = newUsersList;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { FormComponent } from './components/form/form.component';
+import { UserTableComponent } from './components/user-table/user-table.component';
+import { SearchValueComponent } from './components/search-value/search-value.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormComponent,
+    UserTableComponent,
+    SearchValueComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
