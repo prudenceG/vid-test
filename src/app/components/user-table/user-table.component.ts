d007176a9f1b3020634e92a9492ce170531f4cf0
Prudence Gaboriau <prudence.gaboriau@ext.vallourec.com>
2020-10-27 12:41:40 +0100

    feat(form-user): adding table component to display each line filtered or not and delete an item

---
+++ src/app/components/user-table/user-table.component.ts
@@ -0,0 +1,31 @@
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { User } from 'src/interfaces/user.interface';
+import { Key } from 'src/interfaces/key.interface';
+import { UserInformationsService } from 'src/app/services/user-informations.service';
+
+@Component({
+  selector: 'app-user-table',
+  templateUrl: './user-table.component.html',
+  styleUrls: ['./user-table.component.scss']
+})
+export class UserTableComponent {
+  @Input() users: User[];
+
+  @Input() filteredUsers: User[];
+
+  @Input() keys: Key[];
+
+  @Output() removeUserEvent = new EventEmitter();
+
+  constructor(
+    private userInformationService: UserInformationsService,
+  ) {}
+
+  removeUser(user): void {
+    const newUsers = this.userInformationService.filterUserListById(user, this.users);
+
+    localStorage.setItem('users', JSON.stringify(newUsers));
+    this.removeUserEvent.emit(newUsers);
+    this.filteredUsers = this.userInformationService.filterUserListById(user, this.filteredUsers);
+  }
+}
